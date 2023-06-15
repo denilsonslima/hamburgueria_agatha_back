bff63bd7147cdd9ffdd2cd3eefdd2d97e6c609b4
Denilson Lima <denilsondslima@gmail.com>
2023-06-14 23:12:11 -0300

    feat: config db

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,3 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-export let prisma: PrismaClient;
-export function connectDb(): void {
-  prisma = new PrismaClient();
-}
-
-export async function disconnectDB(): Promise<void> {
-  await prisma?.$disconnect();
-}
+export const prisma = new PrismaClient();
